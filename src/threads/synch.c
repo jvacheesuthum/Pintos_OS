83305a852a83e65859373030f5d86d0666b1a891
hcl114 <hcl114@ic.ac.uk>
2016-02-03 05:22:14 +0000

    cond signal priority

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -116,7 +116,6 @@ sema_up (struct semaphore *sema)
 
   struct thread *highest = NULL;
   if (!list_empty (&sema->waiters)) {
-    //thread_unblock (list_entry (list_pop_front(&sema->waiters), struct thread, elem));
     struct list_elem* e;
     e = list_begin (&sema->waiters);
     highest = list_entry (e, struct thread, elem);
@@ -129,8 +128,6 @@ sema_up (struct semaphore *sema)
       e = list_next(e);
     }
     list_remove (&(highest->elem));
-    if (highest->priority > thread_current ()->priority)
-      printf("should see higher!\n");
 
     thread_unblock (highest);
 
@@ -274,6 +271,7 @@ struct semaphore_elem
   {
     struct list_elem elem;              /* List element. */
     struct semaphore semaphore;         /* This semaphore. */
+    int priority;
   };
 
 /* Initializes condition variable COND.  A condition variable
@@ -318,6 +316,7 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
   
   sema_init (&waiter.semaphore, 0);
+  waiter.priority = thread_current ()->priority;
   list_push_back (&cond->waiters, &waiter.elem);
   lock_release (lock);
   sema_down (&waiter.semaphore);
@@ -339,9 +338,24 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
   ASSERT (!intr_context ());
   ASSERT (lock_held_by_current_thread (lock));
 
-  if (!list_empty (&cond->waiters)) 
-    sema_up (&list_entry (list_pop_front (&cond->waiters),
-                          struct semaphore_elem, elem)->semaphore);
+  struct semaphore_elem *highest = NULL;
+  if (!list_empty (&cond->waiters)) {
+    struct list_elem* e;
+    e = list_begin (&cond->waiters);
+    highest = list_entry (e, struct semaphore_elem, elem);
+    struct semaphore_elem *s = NULL;
+    while (e != list_end (&cond->waiters)) {
+      s = list_entry (e, struct semaphore_elem, elem);
+      if (s->priority > highest->priority) {
+	highest = s;
+      }
+      e = list_next(e);
+    }
+    list_remove (&(highest->elem));
+
+    sema_up (highest->semaphore);
+
+  }
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
