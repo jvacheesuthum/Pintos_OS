c438b469eb6eca9d08da5d020183823d4ad177d6
hcl114 <hcl114@ic.ac.uk>
2016-02-03 05:46:43 +0000

    malloc queues

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -23,7 +23,7 @@
 
 /* List of processes in THREAD_READY state, that is, processes
    that are ready to run but not actually running. */
-static struct list *ready_queue;
+static struct list *ready_queue[64];
 
 /* List of all processes.  Processes are added to this list
    when they are first scheduled and removed when they exit. */
@@ -92,10 +92,10 @@ thread_init (void)
 
   lock_init (&tid_lock);
   
-  ready_queue = malloc(64* sizeof(struct list));
   int i;
   for (i = 0; i < 64; i++) {
-    list_init (&(ready_queue[i]));
+    ready_queue[i] = malloc(sizeof(struct list));
+    list_init (ready_queue[i]);
   }
 
   list_init (&all_list);
