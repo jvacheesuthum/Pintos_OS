408960d49c45ff3705adc20b42dd0b11b4d974a0
hcl114 <hcl114@ic.ac.uk>
2016-02-03 03:52:05 +0000

    sema printf debug

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -117,11 +117,13 @@ sema_up (struct semaphore *sema)
     struct thread *t = list_entry (list_pop_front(&sema->waiters),
 					 struct thread, elem);
     thread_unblock (t);
-    
+    printf("thread unblocked in sema\n");
     if (t->priority > (thread_current ()->priority)) {
+      printf("sema has high priority\n");
       thread_yield ();
     }
   }
+  printf("sema done\n");
   sema->value++;
   intr_set_level (old_level);
 }
