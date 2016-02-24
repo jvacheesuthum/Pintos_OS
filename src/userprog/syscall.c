e8234fc41a4863279f1c937e493c93abfade5b03
Vasin W <vasin@Vasins-MacBook-Pro.local>
2016-02-24 02:54:41 +0000

    infrastructure first implementation, not yet checked

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -13,8 +13,30 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  /*
+   * The caller’s stack pointer is accessible to syscall_handler() as the ‘esp’ member 
+   * of the struct intr_frame passed to it. (struct intr_frame is on the kernel stack.)
+   */
+
+  // refer to page37 specs // or use esp as int* sys_name as seen in lib/user/syscall.c
+  void *esp   = f->esp;
+  int argc    = *(*esp + 4); //check if +4 is correct? the address for argc is at address esp + 4 
+  char** argv = *(*esp + 4*2);
+  char* syscall_name = *argv; 
+  char* args[argc];
+  int i;
+  for(i=0; i<argc; i++){
+    args[i] = *(argv + (i+1)*4);
+  }
+
+  switch(syscall_name){
+    case syscall_name == SYS_EXIT:
+      exit();
+      break;
+    case default: // original code
+      printf("syscall!");
+      thread_exit ();
+  }
 }
