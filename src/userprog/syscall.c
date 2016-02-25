0e8d2e6a54c71c1e9ad17a3ba54c595e6aca8f19
jiranart <jiranart.vacheesuthum12@imperial.ac.uk>
2016-02-25 02:39:11 +0000

    write and partial exit implemented

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -32,8 +32,11 @@ syscall_handler (struct intr_frame *f)
   }
 
   switch(syscall_name){
-    case syscall_name == SYS_EXIT:
-      exit();
+    case SYS_EXIT:
+      exit(args[0]);   //think this is the right arg for exit -> return afterwards?
+      break;
+    case SYS_WRITE:
+      write(args[0], args[1], args[2]);
       break;
     case default: // original code
       printf("syscall!");
@@ -43,12 +46,37 @@ syscall_handler (struct intr_frame *f)
   //------- write your methods here --------//
   void
   exit (int status){
+    thread_exit();    //in thread.c which calls process_exit() in process.c
+    //send status to kernel ?????????? HOWWWWWWWWWWWWWWWWWWWWW
     return;
   }
   
+//putbuf() in specs. lib/kernel/console.c via stdio.h***
   int
   write (int fd, const void *buffer, unsigned size) {
-    return 0;
+    //maybe check if buffer pointer is valid here
+    char* data;
+    data = buffer;
+    unsigned wsize = size;
+    switch(fd){
+      case 1 : 			//write to sys console
+	const int maxbufout = 300;
+	if (size > maxbufout) { 	//break up large buffer (>300 bytes for now)
+	  unsigned wsize = size;
+	  char* temp;
+	  while (wsize > maxbufout) {
+	    strlcpy(temp, data, maxbufout);
+	    putbuf(temp, maxbufout);		       
+	    data += maxbufout;       //cuts the first 300 char off data
+	    wsize -= maxbufout;
+	  }
+	}	
+	putbuf(buffer, wsize);      //if goes to if clause above this prints last section of buffer which is < maxbufout
+	return size;
+      case default :
+	//return file_write( 'convert fd to file* here', buffer, size);  //in file.c
+	break;
+    }
   }
   
   
